962697787c40b05a4435f83ae4e2b377c2eb71f3
Josh Hoffer <jhoffer@sansorgan.es>
2022-02-10 19:48:54 -0700

    use node inside FHS, proper multiple vscode directory support

--- modules/module.nix
+++ modules/module.nix
@@ -3,6 +3,50 @@ moduleConfig:
 
 with lib;
 
+let
+    originalNodePackage = pkgs.nodejs-14_x;
+
+  # Adapted from https://github.com/NixOS/nixpkgs/blob/nixos-unstable/pkgs/applications/editors/vscode/generic.nix#L181
+  nodePackageFhs = pkgs.buildFHSUserEnv {
+    name = originalNodePackage.name;
+
+    # additional libraries which are commonly needed for extensions
+    targetPkgs = pkgs: (with pkgs; [
+      # ld-linux-x86-64-linux.so.2 and others
+      glibc
+
+      # dotnet
+      curl
+      icu
+      libunwind
+      libuuid
+      openssl
+      zlib
+
+      # mono
+      krb5
+    ]);
+
+    runScript = "${originalNodePackage}/bin/node";
+
+    meta = {
+      description = ''
+        Wrapped variant of ${name} which launches in an FHS compatible envrionment.
+        Should allow for easy usage of extensions without nix-specific modifications.
+      '';
+    };
+  };
+
+  originalNodePackageBin = "${originalNodePackage}/bin/node";
+  nodePackageFhsBin = "${nodePackageFhs}/bin/${nodePackageFhs.name}";
+
+  nodeBinToUse = if 
+    config.services.vscode-server.useFhsNodeEnvironment
+  then 
+    nodePackageFhsBin
+  else
+    originalNodePackageBin;
+in
 {
   options.services.nixos-vscode-server = with types;{
     enable = mkEnableOption "auto-fix service for vscode-server in NixOS";
@@ -10,71 +54,57 @@ with lib;
       type = package;
       default = pkgs.nodejs-14_x;
     };
-    ripPackage = mkOption {
-      type = package;
-      default = pkgs.ripgrep;
-    };
-    findPackage = mkOption {
-      type = package;
-      default = pkgs.findutils;
+    useFhsNodeEnvironment = mkOption {
+      type = types.bool;
+      default = false;
+      example = true;
+      description = ''
+        Wraps NodeJS in a Fhs compatible envrionment. Should allow for easy usage of extensions without nix-specific modifications. 
+      '';
     };
   };
 
-  config =
-    let
-      cfg = config.services.nixos-vscode-server;
-      nodePath = "${cfg.nodePackage}/bin/node";
-      findPath = "${cfg.findPackage}/bin/find";
-      ripPath = "${cfg.ripPackage}/bin/rg";
-      mkStartScript = name: pkgs.writeShellScript "${name}.sh" ''
+  config = moduleConfig rec {
+    name = "auto-fix-vscode-server";
+    description = "Automatically fix the VS Code server used by the remote SSH extension";
+    serviceConfig = {
+      Restart = "always";
+      RestartSec = 0;
+      ExecStart = "${pkgs.writeShellScript "${name}.sh" ''
         set -euo pipefail
-        PATH=${makeBinPath (with pkgs; [ coreutils inotify-tools ])}
-        bin_dir=~/.vscode-server/bin
-        bin_dir2=~/.vscode-server-insiders/bin
-        if [[ -e $bin_dir2 ]]; then
-          ${findPath} "$bin_dir2" -mindepth 2 -maxdepth 2 -name node -type f -exec ln -sfT ${nodePath} {} \;
-          ${findPath} "$bin_dir2" -path '*/bin/rg' -exec ln -sfT ${ripPath} {} \;
-        fi
-        if [[ -e $bin_dir ]]; then
-          ${findPath} "$bin_dir" -mindepth 2 -maxdepth 2 -name node -type f -exec ln -sfT ${nodePath} {} \;
-          ${findPath} "$bin_dir" -path '*/bin/rg' -exec ln -sfT ${ripPath} {} \;
-        fi
-        mkdir -p "$bin_dir"
+        PATH=${makeBinPath (with pkgs; [ coreutils findutils inotify-tools ])}
+        fix_vscode () {
+            bin_dir="$1"
+            if [[ -e $bin_dir ]]; then
+              find "$bin_dir" -mindepth 2 -maxdepth 2 -name node -exec ln -sfT ${nodeBinToUse} {} \;
+              find "$bin_dir" -path '*/bin/rg' -exec ln -sfT ${pkgs.ripgrep}/bin/rg {} \;
+            else
+              mkdir -p "$bin_dir"
+            fi
+        }
+        dirs=(
+          "~/.vscode-server/bin"
+          "~/.vscode-server-insiders/bin"
+        )
+        fix_all_vscode() {
+          for bin_dir in $dirs ; do
+            fix_vscode "$bin_dir"
+        }
+
         while IFS=: read -r bin_dir event; do
           # A new version of the VS Code Server is being created.
           if [[ $event == 'CREATE,ISDIR' ]]; then
             # Create a trigger to know when their node is being created and replace it for our symlink.
             touch "$bin_dir/node"
             inotifywait -qq -e DELETE_SELF "$bin_dir/node"
-            if [[ -e $bin_dir2 ]]; then
-              ${findPath} "$bin_dir2" -mindepth 2 -maxdepth 2 -name node -type f -exec ln -sfT ${nodePath} {} \;
-              ${findPath} "$bin_dir2" -path '*/bin/rg' -exec ln -sfT ${ripPath} {} \;
-            fi
-            if [[ -e $bin_dir ]]; then
-              ${findPath} "$bin_dir" -mindepth 2 -maxdepth 2 -name node -type f -exec ln -sfT ${nodePath} {} \;
-              ${findPath} "$bin_dir" -path '*/bin/rg' -exec ln -sfT ${ripPath} {} \;
-            fi
+            fix_vscode $bin_dir
           # The monitored directory is deleted, e.g. when "Uninstall VS Code Server from Host" has been run.
           elif [[ $event == DELETE_SELF ]]; then
             # See the comments above Restart in the service config.
             exit 0
           fi
-        done < <(inotifywait -q -m -e CREATE,ISDIR -e DELETE_SELF --format '%w%f:%e' "$bin_dir")
-      '';
-    in
-      mkIf cfg.enable (
-        moduleConfig rec {
-          name = "nixos-vscode-server";
-          description = "Automatically fix the VS Code server used by the remote SSH extension";
-          serviceConfig = {
-            # When a monitored directory is deleted, it will stop being monitored.
-            # Even if it is later recreated it will not restart monitoring it.
-            # Unfortunately the monitor does not kill itself when it stops monitoring,
-            # so rather than creating our own restart mechanism, we leverage systemd to do this for us.
-            Restart = "always";
-            RestartSec = 0;
-            ExecStart = "${mkStartScript name}";
-          };
-        }
-      );
+        done < <(inotifywait -q -m -e CREATE,ISDIR -e DELETE_SELF --format '%w%f:%e' "''${dirs[@]}")
+      ''}";
+    };
+  };
 }
