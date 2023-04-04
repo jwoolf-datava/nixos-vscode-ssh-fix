ed2308f0465aadc5efeddd812c3f955eb7329517
Josh Hoffer <jhoffer@sansorgan.es>
2023-04-04 17:21:04 -0600

    phpstorm module: switch to vscode style patcher

--- modules/phpstorm.nix
+++ modules/phpstorm.nix
@@ -3,28 +3,7 @@ moduleConfig:
 
 with lib;
 
-let
-  phpStormPatched = with pkgs; jetbrains.phpstorm.overrideAttrs (old: {
-      patches = (old.patches or []) ++ [
-        ./phpstorm.patch
-      ];
-
-      installPhase = (old.installPhase or "") + ''
-        makeWrapper "$out/$pname/bin/remote-dev-server.sh" "$out/bin/$pname-remote-dev-server" \
-          --prefix PATH : "$out/libexec/$pname:${pkgs.lib.makeBinPath [ pkgs.jdk pkgs.coreutils pkgs.gnugrep pkgs.which pkgs.git ]}" \
-          --prefix LD_LIBRARY_PATH : "${pkgs.lib.makeLibraryPath ([
-            # Some internals want libstdc++.so.6
-            pkgs.stdenv.cc.cc.lib pkgs.libsecret pkgs.e2fsprogs
-            pkgs.libnotify
-          ])}" \
-          --set-default JDK_HOME "$jdk" \
-          --set-default ANDROID_JAVA_HOME "$jdk" \
-          --set-default JAVA_HOME "$jdk" \
-          --set PHPSTORM_JDK "$jdk" \
-          --set PHPSTORM_VM_OPTIONS ${old.vmoptsFile}
-      '';
-    });
-in {
+{
   options.services.phpstorm-fix = with types; {
     enable = mkEnableOption "auto-fix service for phpstorm in NixOS";
   };
@@ -32,11 +11,57 @@ in {
   config = moduleConfig rec {
     name = "auto-fix-phpstorm";
     description = "Automatically fix the PHPStorm RemoteDev.";
-    path = [ phpStormPatched ];
     serviceConfig = {
-      Restart = "no";
+      Restart = "always";
+      RestartSec = "5s";
       ExecStart = "${pkgs.writeShellScript "${name}.sh" ''
-        ${phpStormPatched}/bin/phpstorm-remote-dev-server registerBackendLocationForGateway
+    set -euo pipefail
+    PATH=${makeBinPath (with pkgs; [ coreutils findutils inotify-tools patchelf gnused ])}
+    bin_dir=~/.cache/JetBrains/RemoteDev/dist/
+
+    mkdir -p ~/.cache/JetBrains/RemoteDev/dist/ 
+
+    get_file_size() {
+      fname="$1"
+      echo $(ls -l $fname | cut -d ' ' -f5)
+    }
+    munge_size_hack() {
+      fname="$1"
+      size="$2"
+      strip $fname
+      truncate --size=$size $fname
+    }
+
+    patch_fs_notifier() {
+      interpreter=$(echo ${pkgs.glibc.out}/lib/ld-linux*.so.2)
+      fs_notifier=$1; 
+      
+      if [ -z "$in" ]; then 
+        read fs_notifier; 
+      fi
+
+      target_size=$(get_file_size $fs_notifier)
+      patchelf --set-interpreter "$interpreter" $fs_notifier
+      munge_size_hack $fs_notifier $target_size
+    }
+
+    find "$bin_dir" -mindepth 5 -maxdepth 5 -name launcher.sh -exec sed -i -e 's#LD_LINUX=/lib64/ld-linux-x86-64.so.2#LD_LINUX=${pkgs.glibc.out}/lib/ld-linux-x86-64.so.2#g' {} \;
+    find "$bin_dir" -mindepth 3 -maxdepth 3 -name fsnotifier -exec patch_fs_notifier {} \;
+    find "$bin_dir" -mindepth 3 -maxdepth 3 -name fsnotifier64 -exec patch_fs_notifier {} \;
+
+    while IFS=: read -r out event; do
+      case "$out" in
+        */remote-dev-server/bin) 
+          sed -i 's#LD_LINUX=/lib64/ld-linux-x86-64.so.2#LD_LINUX=${pkgs.glibc.out}/lib/ld-linux-x86-64.so.2#g' "$out/launcher.sh"
+
+          if [[ "${pkgs.stdenv.hostPlatform.system}" == "x86_64-linux" && -e $out/fsnotifier64 ]]; then
+            patch_fs_notifier $out/fsnotifier64 
+          else
+            patch_fs_notifier $out/fsnotifier 
+          fi
+        ;;
+      esac
+    done < <(inotifywait -r -m -q -e CREATE --include '^.*ideaIU[-[:digit:]\.]+(/plugins)?(/remote-dev-server)?(/bin)?$' --format '%w%f:%e' "$bin_dir")
       ''}";
     };
   };
